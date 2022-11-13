bcb1bf22e2ffd34d7fd28a27159563d91dee1179
Sean Falese <falese@gmail.com>
2022-11-12 21:11:13 -0500

    workign blog template, removal of old site

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,43 +1,37 @@
-import * as React from 'react'
-import {Link, useStaticQuery, graphql} from 'gatsby'
-import { container,
-    heading,
-    navLinks,
-    navLinkItem,
-    navLinkText,
-    siteTitle} from './styles.module.scss'
+import * as React from "react"
+import { Link } from "gatsby"
 
-const Layout = ({pageTitle, children}) => {
-    const data = useStaticQuery(
-        graphql`
-        query{
-            site{
-                siteMetadata{
-                    title
-                }
-            }
-        }`
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
+  let header
+
+  if (isRootPath) {
+    header = (
+      <h1 className="main-heading">
+        <Link to="/">{title}</Link>
+       
+      </h1>
+     
     )
-    return(
-        <div className={container}>
-            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-            <nav>
-                <ul className={navLinks}>
-                    <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link> </li>
-                        
-                    <li className={navLinkItem}><Link to="/about" className={navLinkText}>About Us</Link></li>
-                    <li className={navLinkItem}><Link to="/log" className={navLinkText}>Log Entries</Link></li>
-                    <li className={navLinkItem}><Link to="/intern" className={navLinkText}>Apply for an Internship</Link></li>
-                    <li className={navLinkItem}><Link to="/hall" className={navLinkText}>Intern Hall of Fame</Link></li>
-                   
-                </ul>
-            </nav>
-            <main>
-                <h1 className={heading}>{pageTitle}</h1>
-                {children}
-            </main>
-        </div>
+  } else {
+    header = (
+      <Link className="header-link-home" to="/">
+        {title}
+      </Link>
     )
+  }
+
+  return (
+    <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <header className="global-header">{header}</header>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()} Laughing Head Insurance Company
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
--- src/components/seo.js
+++ src/components/seo.js
@@ -1,18 +1,41 @@
-import * as React from 'react'
-import {useStaticQuery, graphql} from 'gatsby'
+/**
+ * SEO component that queries for data with
+ * Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
+ */
 
-const Seo = ({title}) =>{
-    const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+const Seo = ({ description, title, children }) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
         }
       }
-    }
-  `)
-    return(
-        <title>{title} | {data.site.siteMetadata.title}</title>
-    )
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+  const defaultTitle = site.siteMetadata?.title
+
+  return (
+    <>
+      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+      <meta name="description" content={metaDescription} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:type" content="website" />
+      
+      {children}
+    </>
+  )
 }
+
 export default Seo
